97b97f4f8524dc78919e343f8b917d5c5ed24e93
Dani Marín <danii04.mb@gmail.com>
2023-06-06 16:30:28 +0200

    fix some bugs, and add conversations and groups funcionality

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,22 @@
+import getUsers from '../actions/getUsers';
 import Sidebar from '../components/sidebar/Sidebar';
+import UserList from './components/UserList';
 
 export default async function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+	const users = await getUsers();
+
+
   return (
 		// @ts-expect-error Server Component
     <Sidebar>
-      <div className='h-full'>{children}</div>
+      <div className='h-full'>
+				<UserList items={users}/>
+				{children}
+				</div>
     </Sidebar>
   );
 }
